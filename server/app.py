923901ba21ee4e2265cb37f48563de46c286f31b
Kevin Bravo <kbravo1024@gmail.com>
2023-08-30 19:12:43 -0700

    complete routing and views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(f'{parameter}')
+    return f'{parameter}'
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    num = range(parameter)
+    new_list = ''
+    for i in num:
+        new_list += f'{i}\n'
+    return f'{new_list}'
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1,operation,num2):
+    if operation == 'div':
+        operator = '/'
+    else:
+        operator = operation
+    result = eval(f'{num1}{operator}{num2}')
+    return f'{result}'
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
